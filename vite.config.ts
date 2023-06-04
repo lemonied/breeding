4ebbb245837158393781074b6470b8367a016a26
ChenJiYuan <jiyuan.chen@easystack.cn>
2023-06-05 00:52:04 +0800

    Modify config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,37 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  css: {
-    postcss: {
-      plugins: [
-        require('autoprefixer'),
-      ],
-    },
-  },
-  server: {
-    proxy: {
-      '/ngapi': {
-        target: 'http://106.12.145.25/',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.VITE_BASE_URL || '/',
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
-  },
+    css: {
+      postcss: {
+        plugins: [
+          require('autoprefixer'),
+        ],
+      },
+    },
+    server: {
+      proxy: {
+        '/ngapi': {
+          target: 'http://106.12.145.25/',
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
